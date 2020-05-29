39778bc08829f6cf13cfa86e13873891e4cb5628
rotoslinger <harrison.levi.l@gmail.com>
2020-05-29 14:34:20 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,158 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+2 f f f 2 2 2 2 2 2 2 2 2 2 2 2 
+2 f 2 2 f 2 2 f f f 2 2 2 2 2 2 
+2 f f 2 f f 2 f 2 f 2 f 2 2 2 2 
+2 f 2 2 f 2 2 f 2 f 2 f f f f f 
+2 f 2 2 f 2 2 f f f 2 f 2 f 2 f 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 f 2 f 2 2 2 2 2 2 2 2 2 2 2 2 
+2 f 2 f 2 f 2 2 2 2 2 2 2 2 2 2 
+2 f 2 f 2 2 2 2 2 2 f f f 2 2 2 
+2 f f f 2 f 2 f 2 2 f 2 f 2 2 2 
+2 f 2 f 2 f 2 f f 2 f 2 f 2 2 2 
+2 f 2 f 2 f 2 f 2 2 f f f 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+3 f f f 3 3 3 3 3 3 3 3 3 3 3 3 
+3 f 3 3 3 3 3 f f f 3 f 3 3 3 3 
+3 f f 3 f 3 3 f 3 f 3 f f f f f 
+3 f 3 3 f f 3 f 3 f 3 f 3 f 3 f 
+3 f 3 3 f 3 3 f f f 3 f 3 f 3 f 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 f 3 f 3 3 3 3 3 f 3 3 3 f 3 3 
+3 f 3 f 3 f 3 f 3 f f f 3 3 3 3 
+3 f 3 f 3 f 3 f 3 f f 3 3 f 3 3 
+3 f f f 3 f f f 3 f 3 f 3 f 3 3 
+3 3 3 f 3 3 3 3 3 3 3 3 3 f 3 3 
+3 3 3 f 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 f 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+f f f 6 6 6 6 6 6 6 6 f 6 6 6 6 
+f 6 6 6 f 6 6 f f f 6 f f f f f 
+f f 6 6 f f 6 f 6 f 6 f 6 f 6 f 
+f 6 6 6 f 6 6 f f f 6 f 6 f 6 f 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 f 6 6 f f f 6 6 6 6 6 6 6 6 
+6 6 f 6 6 f 6 f 6 f 6 f 6 6 f 6 
+6 6 f 6 6 f f f 6 f 6 f 6 6 6 6 
+6 6 f 6 6 f 6 6 6 f 6 f 6 6 f 6 
+6 6 f f 6 f f f 6 6 f 6 6 6 f 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 f 6 
+6 2 6 2 6 6 6 6 6 6 6 6 6 6 6 6 
+2 2 2 2 2 6 6 6 6 6 6 6 6 6 6 6 
+2 2 2 2 2 6 6 6 6 6 6 6 6 6 6 6 
+6 2 2 2 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 2 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+f f f 4 4 4 4 4 4 4 4 4 4 4 4 4 
+f 4 4 4 f 4 4 4 4 4 4 f 4 4 4 4 
+f f 4 4 f f 4 f f f 4 f f f f f 
+f 4 4 4 f 4 4 f 4 f 4 f 4 f 4 f 
+f 4 4 4 f 4 4 f f f 4 f 4 f 4 f 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+f f f 4 f f f 4 f f 4 4 4 4 4 4 
+f f 4 4 f 4 f 4 f 4 4 f 4 f 4 4 
+f 4 f 4 f f f 4 f f 4 f 4 f 4 4 
+f 4 f 4 4 4 4 4 4 f 4 f 4 f 4 4 
+4 4 4 4 f f f 4 f f 4 f f f 4 4 
+f 4 4 4 f 4 f 4 4 4 4 4 4 4 4 4 
+f f f 4 f f f 4 4 4 4 4 4 4 4 4 
+f f 4 4 f 4 4 4 4 4 4 4 4 4 4 4 
+f 4 f 4 f f f 4 4 4 4 4 4 4 4 4 
+`
+}
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile1, function (sprite, location) {
+    game.showLongText("happy mothers day! thank you for making all are food and reminding me to do my homework.I love you!", DialogLayout.Bottom)
+})
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile5, function (sprite, location) {
+    game.showLongText("", DialogLayout.Bottom)
+})
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile4, function (sprite, location) {
+    game.showLongText("I love you so much!  We have so many great memories with each other, let's make lots more!", DialogLayout.Bottom)
+})
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
+    game.showLongText("i love you. you are my favourite friend.", DialogLayout.Bottom)
+})
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . f f f f f f f f f f f f f f f f . . . . . . 
+. . . . . . . f f f f f f f f f f f f f f f f f f . . . . . 
+. . . . . . . f f 4 f 1 1 1 f 4 4 f 1 1 1 f 4 f f . . . . . 
+. . . . . . . f f f f 1 f 1 f f f f 1 f 1 f f f f . . . . . 
+. . . . . . . f f 4 f 1 1 1 f 4 4 f 1 1 1 f 4 f f . . . . . 
+. . . . . . . f f 4 f f f f f 4 4 f f f f f 4 f f . . . . . 
+. . . . . . . f f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f f . . . . . 
+. . . . . . . f f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f f . . . . . 
+. . . . . . . f f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f f . . . . . 
+. . . . . . . f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f . . . . . 
+. . . . . . . f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f . . . . . 
+. . . . . . . f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f . . . . . 
+. . . . . f f f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f f f . . . 
+. . . . . f f f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f f f . . . 
+. . . . . f f f . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . f f f . . . 
+. . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . 
+. . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . 
+. . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . 
+. . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . 
+. . . . . . . . . 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.setBackgroundColor(9)
+tiles.setTilemap(tiles.createTilemap(
+            hex`0a0008000000000000000000000000000001000000000000000000000000000000000000000000000002000000000000000000000000000000030000000000000000000000040000000000000000000000000000`,
+            img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile5],
+            TileScale.Sixteen
+        ))
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
